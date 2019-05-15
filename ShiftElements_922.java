830644c8fc95341b0d80f267d8fbec3e9e53dc14
Natalie Datsyuk <35202549+Scoobidoobidoo@users.noreply.github.com>
2019-05-15 17:44:53 +0300

    Update ShiftElements_922.java

--- ShiftElements_922.java
+++ ShiftElements_922.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class ShiftElements_922 {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,9, -6};
+        int[] arr = {1,2,3,4,5,6,7,8};
         int temp = arr [arr.length-1];
            for(int i = arr.length -1; i>0; i--){
            arr [i] = arr [i-1];
